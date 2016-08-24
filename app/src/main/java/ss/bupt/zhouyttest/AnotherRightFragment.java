c20ea11d433c041fa6f40da465fec1d4bea2fe4a
learnself <995881323@qq.com>
2016-08-24 14:23:20 +0800

    Initial commit

---
+++ app/src/main/java/ss/bupt/zhouyttest/AnotherRightFragment.java
@@ -0,0 +1,7 @@
+package ss.bupt.zhouyttest;
+
+/**
+ * Created by new on 2016/8/3.
+ */
+public class AnotherRightFragment {
+}
---
+++ fragmentbestpractice/src/main/java/ss/bupt/fragmentbestpractice/NewsTitleFragment.java
@@ -0,0 +1,7 @@
+package ss.bupt.fragmentbestpractice;
+
+/**
+ * Created by new on 2016/8/5.
+ */
+public class NewsTitleFragment {
+}
